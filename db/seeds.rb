e3cb15c3c7c377b7128726580a65c93034db9dc0
Babin Bohora <bohorababin1@gmail.com>
2022-01-17 21:30:52 +0900

    new real data seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 Bookmark.destroy_all
 Movie.destroy_all
 List.destroy_all
+
 puts "seeding"
-movie = []
-movie << Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-movie << Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-movie << Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-movie << Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+movies = []
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.open(url).read
+movies_json = JSON.parse(user_serialized)
+movies_json["results"].each do |movie|
+  movies << Movie.create!(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+    )
+end
 5.times do 
   list = List.create(name: Faker::Name.first_name)
   (0...4).to_a.each do |x|
-    puts movie[x]
-    Bookmark.create!(
+    book = Bookmark.new(
       comment: Faker::Movies::BackToTheFuture.quote,
-      movie: movie[x],
+      movie: movies.sample,
       list: list
     )
+    book.save if book.valid?
   end
 end
 puts "done"
