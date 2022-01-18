dafb13754e66d0bd0b34e7669815b28c7b7e1d8b
Babin Bohora <bohorababin1@gmail.com>
2022-01-19 01:26:21 +0900

    home page hover effect fixed

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,8 +1,8 @@
 const circularButtonEffect = () =>   {
-  const buttons = document.querySelectorAll('.circular-button');
+  const buttons = document.querySelectorAll('.list-title');
 
   const image_finder = (e) => {
-    let image_box = e.currentTarget.previousElementSibling;
+    let image_box = e.currentTarget.parentElement.previousElementSibling;
     console.log("working");
     let images = image_box.querySelectorAll("img");
     images.forEach(image => image.classList.toggle("image-hover"));
