023a9420f24539bdecbaa2a1cbb45f90e9cadbab
Babin Bohora <bohorababin1@gmail.com>
2022-01-22 07:35:38 +0900

    minar adjustment

--- app/javascript/components/init_typed.js
+++ app/javascript/components/init_typed.js
@@ -11,14 +11,15 @@ export const typingEffect = () => {
 }
 
 export const bannerTyper = () => {
-  let godlyThing = ['CINEMA IS THE MOST<br>BEAUTIFUL FRAUD IN<br>THE WHOLE WORLD.<br><br>Jean-Luc Godard',
-      'REAL IS GOOD.<br>INTERESTING IS BETTER.<br><br>Stanley Kubrick',
-      "I'm a film addict,<br>absolutely crazy<br>about cinema.<br><br>Somebody",
-      "CINEMA WITHOUT POPCORN<br>IS LIKE JERRY WITHOUT TOM.<br><br>Tom"
+  let godlyThing = ['CINEMA IS THE MOST<br>BEAUTIFUL FRAUD IN<br>THE WHOLE WORLD.<br><br>-  Jean-Luc Godard',
+      'REAL IS GOOD.<br>INTERESTING IS BETTER.<br><br>- Stanley Kubrick',
+      "I'm a film addict,<br>absolutely crazy<br>about cinema.<br><br>- Somebody",
+      "CINEMA WITHOUT POPCORN<br>IS LIKE JERRY WITHOUT TOM.<br><br>-  Tom",
+      'I BELIEVE IN NO TALENT,<br>I JUST BELIEVE IN<br>HARD-WORK AND DEDICATION.<br><br>- Something'
     ]
   let options = {
     strings: [godlyThing[[Math.floor(Math.random() * godlyThing.length)]]],
-    typeSpeed: 30,
+    typeSpeed: 50,
     loop: false,
     showCursor: false,
     contentType: 'html'
