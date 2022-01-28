7a36bc199fd6a42f38cd1ba74052d0bf08a742d2
Babin Bohora <bohorababin1@gmail.com>
2022-01-28 16:41:10 +0900

    list can be deleted by its owner

--- app/policies/list_policy.rb
+++ app/policies/list_policy.rb
@@ -14,6 +14,16 @@ class ListPolicy < ApplicationPolicy
   end
 
   def edit?
+    record_owner?
+  end
+
+  def destroy?
+    record_owner?
+  end
+
+  private
+
+  def record_owner?
     user == record.user
   end
 end
